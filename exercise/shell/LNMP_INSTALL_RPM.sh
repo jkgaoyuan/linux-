1086998a74aa0981ee3940f0d6465412260ec18c
student <jkgaoyuan@gmail.com>
2019-06-05 20:34:10 +0800

    6.1

---
+++ exercise/shell/LNMP_INSTALL_RPM.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+    echo "安装LNMP并设置默认开机自启;;;"
+    read -p "若要继续请输入1  " yes
+if [ -z $yes ]; then
+
+    echo "请确认nginx的目录是否存在并且已经安装 gcc pcre-devel openssl-devel 若没有会自动安装 "
+        yum -y install gcc pcre-devel openssl-devel
+    read -p "输入nginx存放绝对路径" url_nginx
+
+        cd $url_nginx
+        cat auto/options | grep YES
+    read -p "输入要附加安装的模块用 '\'分隔 或者指定安装路径 或者 其他 若参数为空将默认安装" plug
+        ./configure \
+        $plug
+        make && make install
+#使用yum 安装 提前配置好yum源
+#服务端使用httpd
+#        yum -y install httpd
+        yum -y install mariadb mariadb-devel mariadb-server
+        yum -y install php php-mysql
+
+        #systemctl start httpd
+        systemctl enable httpd
+        systemctl start mariadb
+        systemctl enable mariadb
+        /usr/local/nginx/sbin/nginx
+        echo /usr/local/nginx/sbin/nginx >> /etc/rc.local
+#给予 执行权限.若发现开机无法自己则考虑在 /etc/rc.d/rc.local 中添加 /usr/local/nginx/sbin/nginx 并给予x权限
+        chmod +x /etc/rc.local
+   else
+   exit 2
+fi
