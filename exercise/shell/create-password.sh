c6beec7ed2d53decce36bc2e7975b9ad37bccc0a
student <jkgaoyuan@gmail.com>
2019-06-12 19:22:35 +0800

    6.1

--- exercise/shell/create-password.sh
+++ exercise/shell/create-password.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 str="abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-pass=""
+pass=""                    ###密码初始为空
 for i in {1..8} ; do
-    num=$[RANDOM%${#str}]
-    tmp=${str:$num:1}
+    num=$[RANDOM%${#str}]  ###对随机数使用 str 字符数 求余 使产生的随机数小于小于str字符数量
+    tmp=${str:$num:1}      ###从str 中取一位字符 开始位置是 num
     pass+=$tmp
 done
 echo $pass
