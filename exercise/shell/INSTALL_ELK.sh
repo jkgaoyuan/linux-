6aa143ac901aa7621fd5888943fa4a04f4160f95
student <jkgaoyuan@gmail.com>
2019-07-16 19:41:15 +0800

    7.2

--- exercise/shell/INSTALL_ELK.sh
+++ exercise/shell/INSTALL_ELK.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+cat << EOF
+请确保ip/hosts文件已经配置好,可能会出现错误
+EOF
 for i in  es2 es3 es4 ; do
     rsync -gpo /etc/yum.repos.d/local.repo root@$i:/etc/yum.repos.d/local.repo
     rsync /etc/hotst root@$i:/etc/hosts
