5f805be4187e814ffc73c79926aeae339a36da20
jkgaoyuan <jkgaoyuan@gmail.com>
2019-09-12 20:04:42 +0800

    6.1

--- exercise/shell/cat_nginx_log.sh
+++ exercise/shell/cat_nginx_log.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 logpath='/usr/local/nginx/logs/'
 mv ${logpath}access.log ${logpath}access_$(date -d "yesterday" +"%Y%d%m").log
-mv ${logs_path}access.log ${logs_path}access_$(date -d "yesterday" +"%Y%m%d").log
+#mv ${logs_path}access.log ${logs_path}access_$(date -d "yesterday" +"%Y%m%d").log
 kill -USR1 'cat /usr/local/nginx/logs/nginx.pid'
 #发信号给 pid 让重新生成 日志文件
 kill -USR1  `cat /usr/local/nginx/logs/nginx.pid`
+#`` 表述获取指令的输出结果
 #echo ${logpath}${filename}
 #echo  ${logpath}access_$(date -d "yesterday" +"%Y%m%d").log
