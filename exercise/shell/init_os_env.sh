30c037f4997bf964f3732c4ffc726e9b9b4cd789
jkgaoyuan <jkgaoyuan@gmail.com>
2019-09-17 12:53:43 +0800

    6.1

---
+++ exercise/shell/init_os_env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash+
+##这个脚本是为了 生成 适合自己环境
+yum -y install vim net-tools iproute psmisc tree lftp rsync bash-completion
+reboot
---
+++ exercise/shell/install_php7.0.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+###正在测试的一键安装php 脚本
+yum install -y gcc gcc-c++ make zlib zlib-devel libzip libzip-devel pcre pcre-devel  libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel openssl openssl-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers
+
+yum -y remove libzip
+wget https://nih.at/libzip/libzip-1.2.0.tar.gz
+tar -zxvf libzip-1.2.0.tar.gz
+cd libzip-1.2.0
+./configure
+make && make install
+
+cd ..
+
+tar -xf $1
+a=${$1:0:9}
+cd $a
+
+
